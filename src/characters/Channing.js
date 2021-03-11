2cd3d1e2d081bc5d6f2c27c5fad8ca7420a28f7b
Katrina Dierking <kalynndier@gmail.com>
2021-03-11 18:30:37 -0500

    body markup complete

---
+++ src/characters/Channing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style.css';
+import styled from 'styled-components'
+
+const CharBox = styled.div`
+    border: 1px solid white; 
+    padding: 10%;
+`
+
+function Channing () {
+    return (
+        <CharBox> </CharBox>
+    )
+}
+
+export default Channing; 
